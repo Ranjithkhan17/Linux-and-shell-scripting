a6eb01945794343b51b4c51f85c8a502ef9c96f9
ranjithkumar <ranjithkumar.khan@gmail.com>
2023-01-03 03:28:05 +0530

    Linux commands useful for devops engineer

---
+++ basiclinuxcomands.sh
@@ -0,0 +1,167 @@
+1. sudo command
+
+#Short for superuser do, sudo is one of the most popular basic Linux commands that lets you perform tasks that require administrative or root permissions
+
+2.pwd command
+
+#Use the pwd command to find the path of your current working directory.
+
+3.cd command 
+
+#Use the pwd command to find the path of your current working directory.
+
+4.ls command
+
+#The ls command lists files and directories within a system.
+
+5.cp command
+
+#Use the cp command to copy files or directories and their content.
+
+6.mv command
+
+#The primary use of the mv command is to move and rename files and directories. Additionally, it doesn’t produce an output upon execution.
+
+7.mkdir command
+
+#Use the mkdir command to create one or multiple directories at once and set permissions for each of them.
+
+9.rmdir command
+
+#To permanently delete an empty directory, use the rmdir command.
+
+10.rm command
+
+#The rm command is used to delete files within a directory.
+
+11.touch command
+
+#The touch command allows you to create an empty file or generate and modify a timestamp in the Linux command line.
+
+12.locate command
+
+#The locate command can find a file in the database system.
+
+13.find command
+
+#Use the find command to search for files within a specific directory and perform subsequent operations.
+
+14.grep command
+
+#Another basic Linux command on the list is grep or global regular expression print. It lets you find a word by searching through all the texts in a specific file
+
+15.df command
+
+#Use the df command to report the system’s disk space usage, shown in percentage and kilobyte (KB).
+
+16. du command
+
+#If you want to check how much space a file or a directory takes up, use the du command. You can run this command to identify which part of the system uses the storage excessively.
+
+17.tail command
+
+#The tail command displays the last ten lines of a file. It allows users to check whether a file has new data or to read error messages.
+
+18.head command
+
+#The head command allows you to view the first ten lines of a text. 
+
+19.tar command
+
+#The head command allows you to view the first ten lines of a text. 
+
+20.diff command
+
+#Short for difference, the diff command compares two contents of a file line by line. After analyzing them, it will display the parts that do not match.
+
+21.chown command
+
+#The chown command lets you change the ownership of a file, directory, or symbolic link to a specified username.
+
+22.chmod command
+
+#chmod is a common command that modifies a file or directory’s read, write, and execute permissions. In Linux, each file is associated with three user classes – owner, group member, and others.
+
+
+23.job command
+
+#A job is a process that the shell starts. The jobs command will display all the running processes along with their statuses.
+
+24.kill command
+
+#Use the kill command to terminate an unresponsive program manually. It will signal misbehaving applications and instruct them to close their processes.
+
+25.su command
+
+#To switch user account.
+
+26.uname command
+
+#The uname or unix name command will give detail information about linux system and hardware.
+
+27.ping command
+
+#To check whether server is reachable or not and also trobleshoot the network issue.
+
+28.wget command
+
+#Use this command to download the files from internet
+
+29.top command
+
+#Display all running process and dynamic real-time view of the system.
+
+30.history command
+
+#List out the all executed commands until.
+
+31.Man command
+
+# Man command provides a user manual of any commands.
+
+32.echo command
+
+#The echo command  is used to print the text or strings
+
+33.zip command
+
+#Use to compress the file into a zip file.It also useful for archiving files and directories.
+
+34.hostname command
+
+#To know the system name
+
+35.useradd command
+
+#To add user account
+
+36.passwd command
+
+#To add password for new user
+
+37.userdel command
+
+#To delete the user
+
+38.apt-get command
+
+#apt-get is a command line tool for handling Advanced Package Tool (APT) libraries in Linux.Manage, update, remove, and install software and its dependencies.
+
+39.nano,vi commands
+
+#The nano,vi commands are text editor commands
+
+40.alias command
+
+#This command is allows to  create a shortcut  with the same functionality as a command,file name or text.
+
+
+41.htop command
+
+#The htop command is an interactive program that monitors system resources and server processes in real time.
+
+42.ps command
+
+#The process status or ps command produces a snapshot of all running processes in your system.
+
+
